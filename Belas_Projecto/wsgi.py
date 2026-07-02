7605ee9ab21c436323e2a4cf4531a96e3017c5d4
Salomão Abrantes <salomaoabrantes6@gmail.com>
2026-07-02 07:53:30 +0100

    Movido criador de admin para o wsgi e limpo o build

--- Belas_Projecto/wsgi.py
+++ Belas_Projecto/wsgi.py
@@ -1,16 +1,22 @@
-"""
-WSGI config for Belas_Projecto project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Belas_Projecto.settings')
 
 application = get_wsgi_application()
+
+# --- CÓDIGO DE CRIAÇÃO AUTOMÁTICA DO ADMIN ---
+try:
+    from global_models.models import Usuario
+    
+    username = 'admin'
+    email = 'admin@email.com'
+    senha = '1234'
+    
+    if not Usuario.objects.filter(username=username).exists():
+        print("Criando superusuário no banco de produção...")
+        Usuario.objects.create_superuser(username=username, email=email, password=senha)
+        print("Superusuário criado com sucesso!")
+except Exception as e:
+    print(f"Aviso na criação do superusuário: {e}")
+# ---------------------------------------------
