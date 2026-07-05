f93bb60e49256a5321a36951d532c1bf467d832f
Salomão Abrantes <salomaoabrantes6@gmail.com>
2026-07-05 09:21:14 +0100

    Fix: Forçada configuração explícita e mudar as valores do cloud_name para o uploader do Cloudinary

--- Belas_Projecto/settings.py
+++ Belas_Projecto/settings.py
@@ -156,16 +156,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 import cloudinary
 cloudinary.config(
     cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME', 'turismoImagens'),
-    api_key = os.environ.get('CLOUDINARY_API_KEY', '752713951376889'),
-    api_secret = os.environ.get('CLOUDINARY_API_SECRET', 't3SnQ08x_jSQRrdNXPKO9xTzf6U'),
+    api_key = os.environ.get('CLOUDINARY_API_KEY', '173417643298899'),
+    api_secret = os.environ.get('CLOUDINARY_API_SECRET', 'uh94u-nBfgVQGq-f-qOm9eM008g'),
     secure = True
 )
 
 # 2. Mantido para o pacote django-cloudinary-storage
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', 'turismoImagens'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY', '752713951376889'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', 't3SnQ08x_jSQRrdNXPKO9xTzf6U'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY', '173417643298899'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', 'uh94u-nBfgVQGq-f-qOm9eM008g'),
 }
 
 # Configuração estável de Backends para Django 6.x
